919a415d5d7321835d6f7a8bd937e8a800a13db7
seyelich <robot11148@gmail.com>
2024-02-03 20:11:17 +0700

    refactor:change price page tabs

--- src/pages/PricePage/index.tsx
+++ src/pages/PricePage/index.tsx
@@ -1,26 +1,47 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { Flex, Tabs } from 'antd'
+import Text from 'antd/es/typography/Text'
 import { OrderForm } from 'components/Forms/OrderForm'
 import { PriceForm } from 'components/Forms/PriceForm/index'
 import { PriceTable } from 'components/Tables/PriceTable'
 import { PageTemplate } from 'pages'
 import type { TabsProps } from 'antd'
 
+const labelStyle = {
+  background: 'linear-gradient(to top, #369, #3498db)',
+  borderRadius: 20,
+  padding: 10,
+  height: 50,
+  width: 150,
+}
+
 const items: TabsProps['items'] = [
   {
     key: 'noTax',
-    label: 'Без НДС',
+    label: (
+      <Flex align="center" justify="center" style={labelStyle}>
+        <Text style={{ color: 'white' }}>Без НДС</Text>
+      </Flex>
+    ),
     children: <PriceTable />,
   },
   {
     key: 'tax',
-    label: 'С НДС',
+    label: (
+      <Flex align="center" justify="center" style={labelStyle}>
+        <Text style={{ color: 'white' }}>С НДС</Text>
+      </Flex>
+    ),
     children: <PriceTable tax />,
   },
   {
     key: 'calculate',
-    label: 'Рассчитать цену',
+    label: (
+      <Flex align="center" justify="center" style={labelStyle}>
+        <Text style={{ color: 'white' }}>Рассчитать цену</Text>
+      </Flex>
+    ),
     children: (
       <Flex justify="center">
         <PriceForm />
@@ -29,7 +50,11 @@ const items: TabsProps['items'] = [
   },
   {
     key: 'order',
-    label: 'Заказать',
+    label: (
+      <Flex align="center" justify="center" style={labelStyle}>
+        <Text style={{ color: 'white' }}>Заказать</Text>
+      </Flex>
+    ),
     children: (
       <Flex justify="center">
         <OrderForm />
@@ -50,6 +75,7 @@ export const PricePage = () => {
       <Tabs
         defaultActiveKey={state?.action === 'calc' ? 'calculate' : state?.action ? 'order' : undefined}
         items={items}
+        centered
       />
     </PageTemplate>
   )
